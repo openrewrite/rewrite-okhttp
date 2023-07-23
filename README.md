95721b5a6e3e88cf1200e474659ecaddddedcf2f
Tim te Beek <tim@moderne.io>
2023-07-23 17:49:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+![Logo](https://github.com/openrewrite/rewrite/raw/main/doc/logo-oss.png)
+### Migrate OkHttp. Automatically.
+
+[![ci](https://github.com/openrewrite/rewrite-okhttp/actions/workflows/ci.yml/badge.svg)](https://github.com/openrewrite/rewrite-okhttp/actions/workflows/ci.yml)
+[![Apache 2.0](https://img.shields.io/github/license/openrewrite/rewrite-okhttp.svg)](https://www.apache.org/licenses/LICENSE-2.0)
+[![Maven Central](https://img.shields.io/maven-central/v/org.openrewrite.recipe/rewrite-okhttp.svg)](https://mvnrepository.com/artifact/org.openrewrite.recipe/rewrite-okhttp)
+[![Revved up by Gradle Enterprise](https://img.shields.io/badge/Revved%20up%20by-Gradle%20Enterprise-06A0CE?logo=Gradle&labelColor=02303A)](https://ge.openrewrite.org/scans)
+
+### What is this?
+
+This project implements a [Rewrite module](https://github.com/openrewrite/rewrite) that performs common tasks when migrating to new version of OkHttp.  
+
+Browse [a selection of recipes available through this module in the recipe catalog](https://docs.openrewrite.org/recipes/okhttp).
---
+++ src/test/java/org/openrewrite/okhttp/MigrateToOkHttp5Test.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.okhttp;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.config.Environment;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.openrewrite.maven.Assertions.pomXml;
+
+class MigrateToOkHttp5Test implements RewriteTest {
+
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(Environment.builder()
+          .scanRuntimeClasspath("org.openrewrite.okhttp")
+          .build()
+          .activateRecipes("org.openrewrite.okhttp.UpgradeOkHttp5"));
+    }
+
+    @Nested
+    class Dependencies {
+        @Test
+        void mavenDependency5x() {
+            rewriteRun(
+              //language=xml
+              pomXml("""
+                <?xml version="1.0" encoding="UTF-8"?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                  xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                  <modelVersion>4.0.0</modelVersion>
+                  <groupId>com.example</groupId>
+                  <artifactId>demo</artifactId>
+                  <version>0.0.1-SNAPSHOT</version>
+                  <dependencies>
+                    <dependency>
+                      <groupId>com.squareup.okhttp3</groupId>
+                      <artifactId>okhttp</artifactId>
+                      <version>4.11.0</version>
+                    </dependency>
+                  </dependencies>
+                </project>
+                """, spec -> spec.after(actual -> {
+                  Matcher matcher = Pattern.compile("<version>(5\\.0\\.\\d+(-(alpha|beta)\\.\\d+)?)</version>").matcher(actual);
+                  assertTrue(matcher.find(), actual);
+                  return """
+                    <?xml version="1.0" encoding="UTF-8"?>
+                    <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                      xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                      <modelVersion>4.0.0</modelVersion>
+                      <groupId>com.example</groupId>
+                      <artifactId>demo</artifactId>
+                      <version>0.0.1-SNAPSHOT</version>
+                      <dependencies>
+                        <dependency>
+                          <groupId>com.squareup.okhttp3</groupId>
+                          <artifactId>okhttp</artifactId>
+                          <version>%s</version>
+                        </dependency>
+                      </dependencies>
+                    </project>
+                    """.formatted(matcher.group(1));
+              })));
+        }
+    }
+}
