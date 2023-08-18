162a768d2f5db8a10ba2b512aed8b6b7aa8d99f1
joanvr <joan@moderne.io>
2023-08-18 14:06:19 +0200

    do not modify build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,7 @@ dependencies {
     testImplementation("org.openrewrite:rewrite-gradle")
     testImplementation("org.openrewrite:rewrite-maven")
 
-    testImplementation("com.squareup.okhttp3:okhttp:4.11.0")
-
     testImplementation("org.junit.jupiter:junit-jupiter-engine:latest.release")
 
+    testImplementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
